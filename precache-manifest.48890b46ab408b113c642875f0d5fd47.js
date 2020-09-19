361e1a7b42214931c67b503822efa1b5ae1f995f
Joey Nguyen <joey@simpleltc.com>
2020-09-19 13:07:56 -0500

    Updates

---
+++ precache-manifest.48890b46ab408b113c642875f0d5fd47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381bb37be28e37e1a72a570dc4cd6558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681be42a37f0846e470c",
+    "url": "/static/css/2.dd0d3956.chunk.css"
+  },
+  {
+    "revision": "64b67ea75229b42f60e9",
+    "url": "/static/css/main.e1987968.chunk.css"
+  },
+  {
+    "revision": "681be42a37f0846e470c",
+    "url": "/static/js/2.f9b5bf24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9b5bf24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b67ea75229b42f60e9",
+    "url": "/static/js/main.84f29eeb.chunk.js"
+  },
+  {
+    "revision": "7884bc7ad45033e0a25d",
+    "url": "/static/js/runtime-main.955e12d4.js"
+  }
+]);
