276f2be8e8e7fcc1e3c59b8321b317b539054704
Joey Nguyen <joey@simpleltc.com>
2020-09-20 01:51:11 -0500

    Updates

---
+++ precache-manifest.4b41a6212bde60e3882598a4b407acf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b85a6fe064d95942a2cdc30b56e68fe",
+    "url": "/react-components-test-joeynguyen/index.html"
+  },
+  {
+    "revision": "f3697ef4e871a774eb50",
+    "url": "/react-components-test-joeynguyen/static/css/2.dd0d3956.chunk.css"
+  },
+  {
+    "revision": "a92d9169323dadc92001",
+    "url": "/react-components-test-joeynguyen/static/css/main.49917ff2.chunk.css"
+  },
+  {
+    "revision": "f3697ef4e871a774eb50",
+    "url": "/react-components-test-joeynguyen/static/js/2.f3b7eeca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-components-test-joeynguyen/static/js/2.f3b7eeca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92d9169323dadc92001",
+    "url": "/react-components-test-joeynguyen/static/js/main.20ad768e.chunk.js"
+  },
+  {
+    "revision": "64020989ff5df9a6d656",
+    "url": "/react-components-test-joeynguyen/static/js/runtime-main.93e21999.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-components-test-joeynguyen/precache-manifest.f732a8d0966a3cc7d1901eca7725b05f.js"
+  "/react-components-test-joeynguyen/precache-manifest.4b41a6212bde60e3882598a4b407acf6.js"
 );
 
 self.addEventListener('message', (event) => {
