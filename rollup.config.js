b9826c7ca52b20c3d06b04648cb36f1de32300e4
Joey Nguyen <jnn193@gmail.com>
2020-09-21 14:45:42 -0500

    Set up Rollup

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import node_resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import css from 'rollup-plugin-hot-css';
+import { uglify } from 'rollup-plugin-uglify';
+import replace from '@rollup/plugin-replace';
+
+module.exports = {
+  input: './src/components/index.js',
+  output: {
+    dir: './dist',
+    format: 'cjs',
+    assetFileNames: '[name].[hash][extname]',
+    entryFileNames: '[name].[hash].js',
+  },
+  plugins: [
+    node_resolve(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development")
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**'
+    }),
+    css({
+      file: 'styles.css',
+      loaders: ['scss'],
+    }),
+    uglify(),
+  ],
+  external: ['react', 'react-dom', 'prop-types', 'styled-components'],
+}
