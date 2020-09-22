9508cf738dbd3a11c0034022bc2157c647ab7146
Joey Nguyen <jnn193@gmail.com>
2020-09-22 10:41:56 -0500

    remove unused packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
 import node_resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
-import sass from 'rollup-plugin-sass';
 import commonjs from '@rollup/plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
-import css from 'rollup-plugin-hot-css';
-import { uglify } from 'rollup-plugin-uglify';
-import replace from '@rollup/plugin-replace';
 import postcssModules from 'postcss-modules';
 
-
 import pkg from './package.json';
-
-
-const sassOptions = {
-  output(styles, styleNodes) {
-    console.log("styles", styles);
-    fs.mkdirSync('dist/css', { recursive: true }, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
-    styleNodes.forEach((item) => {
-      console.log("styleNode", item);
-      const { id, content } = item;
-      const scssName = id.substring(id.lastIndexOf('/') + 1, id.length);
-      const name = scssName.split('.')[0];
-      fs.writeFileSync(`dist/css/${name}.css`, content);
-    });
-  },
-  processor: css => postcss([
-    autoprefixer({
-      grid: false
-    }),
-    cssnano()
-  ])
-    .process(css)
-    .then(result => result.css)
-};
-
 
 export default {
   input: './src/components/index.js',
@@ -58,7 +22,7 @@ export default {
     }),
     node_resolve(),
     postcss({
-      extract: true, // don't generate a separate CSS file
+      extract: true, // generate a separate CSS file (for CSS Modules)
       modules: true,
       sourceMap: true,
       use: ['sass'],
@@ -80,7 +44,6 @@ export default {
       ]
     }),
     commonjs(),
-    // uglify(),
     filesize(),
   ],
   external: Object.keys(pkg.peerDependencies || {}),
