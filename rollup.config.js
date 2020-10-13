2fb3edaec3e0775d7ecbac91a9afcbc74efb06a8
Joey Nguyen <joey@simpleltc.com>
2020-10-13 11:07:33 -0500

    remove postcss-modules because it was messing up CSS modules class names

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
-import postcssModules from 'postcss-modules';
 
 import pkg from './package.json';
 
@@ -18,33 +17,32 @@ export default {
   plugins: [
     babel({
       babelHelpers: 'bundled',
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**' // only transpile our source code
     }),
     node_resolve(),
     postcss({
-      extract: true, // generate a separate CSS file (for CSS Modules)
-      modules: true,
-      sourceMap: true,
-      use: ['sass'],
-      extensions: ['.css', '.scss'],
-      plugins: [
-        postcssModules({
-          getJSON (cssFileName, json, filePath) {
+      extract: "styles.css", // generate a separate CSS file (for CSS Modules)
+      modules: {
+          getJSON (fileName, json, filePath) {
             fs.mkdirSync('dist/css', { recursive: true }, (err) => {
               if (err) {
                 throw err;
               }
             });
             // we can use this to write files/copy files to /dist
-            const cssName = path.basename(cssFileName, ".css");
+            const cssName = path.basename(fileName, ".css");
             const outputFilePath = path.resolve("./dist/css/" + cssName);
             fs.copyFileSync(filePath, outputFilePath)
           }
-        })
-      ]
+      },
+      sourceMap: true,
+      use: ['sass'],
+      extensions: ['.css', '.scss'],
     }),
     commonjs(),
     filesize(),
   ],
+  // indicate which modules should be treated as external,
+  // not included in your bundle but still required at runtime
   external: Object.keys(pkg.peerDependencies || {}),
 }
